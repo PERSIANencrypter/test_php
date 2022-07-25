1943f19d10dfe6240f69111325826db4845774c3
Nq force <79386552+PERSIANencrypter@users.noreply.github.com>
2022-07-25 10:54:40 +0430

    update index.py

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
+import requests
+
+img_url = "https://persianencrypter.github.io/frame/20220723_032154_0000.jpg"
+req = requests.get(img_url)
+file = open("ok.jpg", "wb")
+file.write(req.content)
+file.close()
+file = open("ok.jpg", "rb")
+data = file.read()
+data = {"realImg": data}
+urlphp = ""
+requests.post(urlphp, data=data)
+
+
+
 
